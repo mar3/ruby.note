72bfa2915605c5e2d5a1b275879dda2ddc92a066
mass10 <irisawa.masaru@gmail.com>
2016-01-17 08:36:18 +0900

    wip

---
+++ code/variables/array/main.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+
+def _main()
+
+	ar = [1, 20, 300, -400, 'abc']
+
+	print '配列まるごと表示: ', ar, "\n"
+	puts ar[-3], "\n"
+	puts ar[500], "\n"
+	puts ar[3], "\n"
+	puts ar[4], "\n"
+
+end
+
+_main()
+
