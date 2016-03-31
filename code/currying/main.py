916e939729a2f97e314ec369f7146c10cc8e12af
mass10 <irisawa.masaru@gmail.com>
2016-03-31 23:35:55 +0900

    wip

---
+++ code/currying/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+
+
+def _get_printer(stream)
+
+	operation = lambda { |x|
+		printf(stream, "%s\n", x)
+	}
+	return operation
+
+end
+
+def _main()
+
+	printer = _get_printer(STDOUT)
+
+	printer.call(1)
+	printer.call(2)
+	printer.call(3)
+	printer.call("Ok.")
+
+end
+
+_main()
