cbb62acc5f21ae7872e02958652fb81f31cfbddb
mass10 <irisawa.masaru@gmail.com>
2016-04-08 20:14:45 +0900

    wip

--- code/database/mongodb/users/lookup.rb
+++ code/database/mongodb/users/lookup.rb
@@ -7,13 +7,11 @@ require 'time'
 
 # Encoding.default_external = Encoding::UTF_8
 
-def _print_main(collection)
+def _main()
 
-	system("clear")
-	print ' ------------------------- '
-	print Time.now().strftime('%Y-%m-%d %H:%M:%S.%L')
-	print "\n"
-
+	Mongo::Logger.logger.level = ::Logger::INFO
+	client = Mongo::Client.new(['127.0.0.1:27017'], :database => 'test')
+	collection = client['db20160325']
 	count = 0
 	collection.find().sort({USER_NAME: 1}).each do |e|
 		e.delete("_id")
@@ -22,21 +20,7 @@ def _print_main(collection)
 		puts JSON.generate(e)
 		count += 1
 	end
-
 	print '', count, ' indices found.', "\n"
-
-end
-
-def _main()
-
-	Mongo::Logger.logger.level = ::Logger::INFO
-	client = Mongo::Client.new(['127.0.0.1:27017'], :database => 'test')
-	collection = client['db20160325']
-
-	while true
-		_print_main collection
-		sleep 1
-	end
 
 end
 
