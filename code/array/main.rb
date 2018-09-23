75e089abdcaedf5acbbb544bc7bb0a69e2b863cb
mass10 <mass10.github@gmail.com>
2018-09-23 21:29:58 +0900

    wip

---
+++ code/array/main.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+def _main()
+
+	items = [1, "レレレ", "東京都千代田区大手町 1-1"]
+
+	print("\#\#\# print \#\#\#\n")
+	print("items are: ", items, ", size is: ", items.size, "\n")
+	print("items[0] is: ", items[0], "\n")
+	print("items[1] is: ", items[1], "\n")
+	print("items[2] is: ", items[2], "\n")
+	print("items[3] is: ", items[3], "\n")
+	print("items[4] is: ", items[4], "\n")
+	print("\n")
+
+	puts("\#\#\# puts \#\#\#\n")
+	puts(items)
+	puts()
+
+end
+
+_main()
+
