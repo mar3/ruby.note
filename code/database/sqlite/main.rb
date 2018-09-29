3a74c51fde71d201bfb481620c344d138a87d3bf
mass10 <mass10.github@gmail.com>
2018-09-29 10:31:52 +0900

    wip

--- code/database/sqlite/main.rb
+++ code/database/sqlite/main.rb
@@ -12,29 +12,70 @@ def _generate_user_id()
 	return SecureRandom.uuid
 end
 
-def _main()
-
-	db = SQLite3::Database.new("test.db")
-	result = db.execute("
-		CREATE TABLE USERS(
-			USER_ID VARCHAR(1000) NOT NULL,
-			EMAIL VARCHAR(1000) NOT NULL,
-			NAME VARCHAR(1000) NOT NULL,
-			PRIMARY KEY(USER_ID)
-		)")
-	print("result is: ", result, "\n");
-
-	result = db.execute("INSERT INTO USERS(USER_ID, EMAIL, NAME) VALUES(?, ?, ?)", _generate_user_id(), "jimi.hendrix@docomo.ne.jp", "Jimi Hendrix")
-	print("result is: ", result, "\n");
-
-	result = db.execute("DROP TABLE USERS")
-	print("result is: ", result, "\n");
-
-	db.close
-
-	puts "--- end ---"
-	puts "Ok."
-
+def _init()
+	connection = SQLite3::Database.new("test.db")
+	sql = "
+	CREATE TABLE USERS(
+		USER_ID VARCHAR(1000) NOT NULL,
+		EMAIL VARCHAR(1000) NOT NULL,
+		NAME VARCHAR(1000) NOT NULL,
+		PRIMARY KEY(USER_ID)
+	)"
+	result = connection.execute(sql)
+	print("[TRACE] result is: ", result, "\n");
+	connection.close
 end
 
-_main()
+def _insert_records()
+	connection = SQLite3::Database.new("test.db")
+	sql = "INSERT INTO USERS(USER_ID, EMAIL, NAME) VALUES(?, ?, ?)"
+	result = connection.execute(sql, _generate_user_id(), "jimi.hendrix@docomo.ne.jp", "Jimi Hendrix")
+	print("[TRACE] result is: ", result, "\n");
+	result = connection.execute(sql, _generate_user_id(), "freddie.mercury@icloud.com", "Freddie Mercury")
+	print("[TRACE] result is: ", result, "\n");
+	result = connection.execute(sql, _generate_user_id(), "mal.waldron@gmail.com", "Mal Waldron")
+	print("[TRACE] result is: ", result, "\n");
+	connection.close
+end
+
+def _enumerate_records()
+	connection = SQLite3::Database.new("test.db")
+	sql = "SELECT * FROM USERS"
+	result = connection.execute(sql)
+	# print("[TRACE] result is: ", result, "\n");
+	result.each { |row|
+		print(row, "\n")
+	}
+	connection.close
+end
+
+def _drop()
+	connection = SQLite3::Database.new("test.db")
+	begin
+		sql = "DROP TABLE USERS"
+		result = connection.execute(sql)
+		print("[TRACE] result is: ", result, "\n");
+	rescue Exception => e
+		print("[TRACE] ", e, "\n")
+	end
+	connection.close
+end
+
+def _main(argv)
+	print("[TRACE] ### start ###\n")
+	begin
+		_drop()
+		_init()
+		_insert_records()
+		_enumerate_records()
+		_drop()
+	rescue Exception => e
+		print("[TRACE] ", e, "\n")
+		print("[ERROR] error!!!\n")
+	ensure
+		print("[TRACE] (ensure)\n")
+	end
+	print("[TRACE] --- end ---\n")
+end
+
+_main(ARGV)
