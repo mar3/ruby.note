7108db3222f12efb294dd1ba3c092b4ff9c8ac42
mass10 <mass10.github@gmail.com>
2018-10-08 23:30:55 +0900

    wip

---
+++ code/oo/main.rb
@@ -0,0 +1,82 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+
+class Logger
+
+	def Logger.trace(*parameters)
+		print(Time.now().strftime('%Y-%m-%d %H:%M:%S.%L'), " [TRACE] ", *parameters, "\n")
+	end
+
+end
+
+class AbstractTask
+
+	def initialize()
+		Logger.trace('<AbstractTask.initialize()>')
+	end
+
+	def invoke()
+		raise('ASSERTION FAILURE! pure virtual function call!')
+	end
+
+end
+
+class Function1 < AbstractTask
+
+	def initialize()
+		super()
+		Logger.trace('<', self.class, '.initialize()>')
+	end
+
+	def invoke()
+		Logger.trace('<', self.class, '.invoke()>')
+	end
+
+end
+
+class Function2 < AbstractTask
+
+	def initialize()
+		super()
+		Logger.trace('<', self.class, '.initialize()>')
+	end
+
+	def invoke()
+		Logger.trace('<', self.class, '.invoke()>')
+	end
+
+end
+
+class Loader
+
+	def initialize()
+	end
+
+	def configure(path)
+	end
+
+	def load(context)
+		f = Function1.new()
+		return f
+	end
+
+end
+
+class Application
+
+	def run()
+		loader = Loader.new()
+		f = loader.load(nil)
+		f.invoke()
+		Logger.trace('このインスタンスは Function1 である => ', f.is_a?(Function1))
+		Logger.trace('このインスタンスは Function2 である => ', f.is_a?(Function2))
+	end
+
+end
+
+def main()
+	app = Application.new()
+	app.run()
+end
+
+main()
