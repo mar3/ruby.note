6e46ef814956c25c4e16c6f2ae99ec99cd9092ca
Masaru Irisawa <mass10.github@gmail.com>
2018-10-09 13:30:42 +0900

    wip

--- code/array/main.rb
+++ code/array/main.rb
@@ -1,23 +1,41 @@
 #!/usr/bin/env ruby
+# coding: utf-8
+
+def _println(*args)
+
+	print(*args)
+	print("\n")
+
+end
+
+def _enum_items()
+
+	items = [1, 'レレレ', '東京都千代田区大手町 1-1']
+	return items
+
+end
 
 def _main()
 
-	items = [1, "レレレ", "東京都千代田区大手町 1-1"]
+	items = _enum_items()
 
-	print("\#\#\# print \#\#\#\n")
-	print("items are: ", items, ", size is: ", items.size, "\n")
-	print("items[0] is: ", items[0], "\n")
-	print("items[1] is: ", items[1], "\n")
-	print("items[2] is: ", items[2], "\n")
-	print("items[3] is: ", items[3], "\n")
-	print("items[4] is: ", items[4], "\n")
-	print("\n")
+	_println('### init ###')
+	_println('item is: [', items.class, ']')
+	_println()
 
-	puts("\#\#\# puts \#\#\#\n")
+	_println('### print ###')
+	_println('items are: ', items, ', size is: ', items.size)
+	_println('items[0] is: ', items[0])
+	_println('items[1] is: ', items[1])
+	_println('items[2] is: ', items[2])
+	_println('items[3] is: ', items[3])
+	_println('items[4] is: ', items[4])
+	_println()
+
+	_println('### puts ###')
 	puts(items)
-	puts()
+	_println()
 
 end
 
 _main()
-
