34976a05e1a9085da00a0655774b0c524858b7ba
mass10 <mass10.github@gmail.com>
2018-10-24 00:25:18 +0900

    wip

--- code/database/dynamodb/sake/list-tables.rb
+++ code/database/dynamodb/sake/list-tables.rb
@@ -2,22 +2,15 @@
 # coding: utf-8
 
 require 'aws-sdk-dynamodb'
-require 'securerandom'
 require 'pp'
 
 
 class Logger
 
 	def Logger.trace(*parameters)
+
 		print(Time.now().strftime('%Y-%m-%d %H:%M:%S.%L'), ' [TRACE] ', *parameters, "\n")
-	end
 
-end
-
-class Generator
-
-	def Generator.new_id()
-		return SecureRandom.uuid()
 	end
 
 end
@@ -25,37 +18,45 @@ end
 class Application
 
 	public def initialize()
+
 		@dynamodb = nil
+
 	end
 
 	def open()
+
 		if @dynamodb != nil
 			return @dynamodb
 		end
 		credentials = Aws::Credentials.new('', '')
 		@dynamodb = Aws::DynamoDB::Client.new(region: 'local', endpoint: 'http://localhost:8000', credentials: credentials)
 		return @dynamodb
+
 	end
 
 	def list_tables()
+
 		dynamodb = open()
-		tables = dynamodb.list_tables
-		print(PP.pp(tables), "\n")
-		tables.item.each do |e|
+		response = dynamodb.list_tables
+		response.table_names.each do |e|
 			print('    ', e, "\n")
 		end
-		print("\n")
+
 	end
 
 	def run()
+
 		list_tables()
+
 	end
 
 end
 
 def main()
+
 	app = Application.new()
 	app.run()
+
 end
 
 main()
