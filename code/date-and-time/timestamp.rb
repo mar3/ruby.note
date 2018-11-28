b312ee27c58e29f62724f94c9377db6fdb2a4c61
mass10 <mass10.github@gmail.com>
2018-11-28 11:10:23 +0900

    wip

--- code/date-and-time/timestamp.rb
+++ code/date-and-time/timestamp.rb
@@ -6,6 +6,7 @@ require 'time'
 
 def _main()
 
+	puts "--- date ---"
 	puts Date.today.to_s
 	puts Date::new(2000, 1, 1).to_s
 	# puts Date::exist?(2016, 1, 1)
@@ -15,6 +16,10 @@ def _main()
 	puts Time.parse('2016-01-01 01:12:34.777').to_s
 	puts Time.parse('2016-02-29 01:12:34.777').to_s
 	puts Time.parse('2016-02-30 01:12:34.777').to_s
+
+	puts "--- time ---"
+	puts Time.new(2018, 12, 31, 24)
+	puts Time.new(2019,  1,  1)
 
 end
 
