f6bc8173a1abbd7daa4db2d88df3b06d3062d1a9
Masaru Irisawa <mass10.github@gmail.com>
2018-12-10 10:46:20 +0900

    wip

---
+++ code/constants/main.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+
+AAA = "AAA"
+
+# warning: already initialized constant AAA
+# AAA = ""
+
+ aaa = "aaa"
+ aaa = "aaa"
+ aaa = "aaa"
+ aaa = "aaa"
+ aaa = "aaa"
+
+def main()
+
+	# dynamic constant assignment
+	# BBB = "bbb"
+
+	puts(AAA)
+
+end
+
+main()
+
